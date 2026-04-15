b31f425559c02f5af517c693b70941f0dc757087
Robert Lippmann <robert.lippmann.development@gmail.com>
2026-04-15 14:32:33 -0400

    refactor: export preprocessor public API in package init

--- experimental/preprocessor/__init__.py
+++ experimental/preprocessor/__init__.py
@@ -1,1 +1,12 @@
 """Experimental preprocessor integration helpers and prompts."""
+
+from .heuristic_precompiler import PrecompileResult, precompile_heuristic
+from .output_validation import parse_precompiler_output
+from .prompt_utils import render_prompt
+
+__all__ = [
+    "PrecompileResult",
+    "parse_precompiler_output",
+    "precompile_heuristic",
+    "render_prompt",
+]
