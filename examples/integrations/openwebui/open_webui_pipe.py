03c1d0def8633b61e9addb24093cc061f0a0cbe6
Robert Lippmann <robert.lippmann.development@gmail.com>
2026-04-28 01:59:35 -0400

    chore: bump openwebui example version to 0.6

--- examples/integrations/openwebui/open_webui_pipe.py
+++ examples/integrations/openwebui/open_webui_pipe.py
@@ -3,7 +3,7 @@ title: Context Compiler Pipe
 author: rlippmann
 author_url: https://github.com/rlippmann/context-compiler
 funding_url: https://github.com/rlippmann/context-compiler
-version: 0.5
+version: 0.6
 requirements: context-compiler>=0.6.11
 
 Minimal Open WebUI Pipe integration for Context Compiler.
