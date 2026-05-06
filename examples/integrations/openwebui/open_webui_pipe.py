1ac95634fce6919c30022b4f3607ac7ed694a512
Robert Lippmann <robert.lippmann.development@gmail.com>
2026-05-06 02:14:37 -0400

    chore: bump openwebui function version metadata

--- examples/integrations/openwebui/open_webui_pipe.py
+++ examples/integrations/openwebui/open_webui_pipe.py
@@ -3,7 +3,7 @@ title: Context Compiler Pipe
 author: rlippmann
 author_url: https://github.com/rlippmann/context-compiler
 funding_url: https://github.com/rlippmann/context-compiler
-version: 0.8.1
+version: 0.8.2
 requirements: context-compiler>=0.6.14
 
 Minimal Open WebUI Pipe integration for Context Compiler.
